dbafee422966852f5531d75d72d5d64f915ee982
mrunmoy <mrunmoy@gmail.com>
2026-09-03 22:28:25 +1000

    build: initialize nix flake, cmake build system, and clang-format

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "libcfuture (Janus): Zero-Heap Lock-Free Future/Promise Framework for Embedded C";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "libcfuture-dev-shell";
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            clang_14
+            gcc11
+            cppcheck
+            clang-tools
+            lcov
+            valgrind
+            gtest
+          ];
+
+          shellHook = ''
+            export CC=clang
+            export CXX=clang++
+            echo "libcfuture development shell initialized."
+          '';
+        };
+      });
+}
